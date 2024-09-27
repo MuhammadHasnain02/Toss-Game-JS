ec17b146ba7994a80581ca1a5d9f81307913c043
Muhammad Hasnain <mrhasnain9009@gmail.com>
2024-09-26 21:28:16 -0700

    Add files via upload

---
+++ Toss-Game.js
@@ -0,0 +1,16 @@
+// First put one name
+// Second put second name
+
+var player1 = prompt('Enter Player Name 1')
+var player2 = prompt('Enter Player Name 2')
+
+var toss = Math.floor() * 2
+
+
+if(toss == 0){
+alert(player1+' have win Toss')
+}
+
+else{
+alert(player2+' have win Toss')
+}
